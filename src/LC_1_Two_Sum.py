4cbb2666cee3580530d9e597b491b80fa18ffd1a
chrbl <chrbl@LAPTOP-0ENP7GH6.fios-router.home>
2021-01-23 23:31:08 -0500

    Full problem and solution to LC_1 Two Sum O(nLogn).
    May work on better solution at a later point or make that a separate
    file. If it is a separate file, the file name will be updated to
    reflect the time complexity

---
+++ src/LC_1_Two_Sum.py
@@ -0,0 +1,48 @@
+'''
+Created on Jan 23, 2021
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+@author: chrbl
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+'''
+
+from typing import List
+
+nums = [2,7,11,15]
+target = 9
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for operator1 in range(0, len(nums)-1):
+            for operator2 in range(operator1+1, len(nums)):
+                if nums[operator1] + nums[operator2] == target:
+                    return [operator1, operator2]
+        
+#From here down is purely testing the example given to see if we have a solution 
+#The left side of the expression is our result, the right side is the correct result
+#for the given example above attributed to 'myList'         
+sol = Solution()
+if (sol.twoSum(nums, target) == [0, 1]):
+    print(True)
+else:
+    print(False)
+    
+    
+''' This solution was successful on LeetCode. Used lines 24-27 (the rest was for working/testing locally and not in browser)
+    Lines: 4, Runtime: 52ms, Memory: 14.5MB, Time Complexity: O(nLogn)
+    Theory behind the solution: 
+    I decided to add each number to each number to its 'right' in the array. This allows me to check every number against every other
+    number and not having to repeat using the commutative property of addition means we can do faster than O(n^2)
+    This is O(nLogn) as the solution takes O(n) time to look through each initial number and takes O(logn) to look through each
+    number remaining in the list. I can confidently say O(logn) as the second for loop uses the variable from the for loop
+    prior    
+'''   
+    
